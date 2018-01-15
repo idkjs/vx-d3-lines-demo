7df9f57ddcaef20d79869294ac19f28986e44635
Alain Armand <aarmand.inbox@gmail.com>
2018-01-15 18:11:34 +0100

    clean up next.config.js

--- next.config.js
+++ next.config.js
@@ -3,13 +3,4 @@ exports.exportPathMap = () => ({
   "/grid": { page: "/grid" },
   "/multiline": { page: "/multiline" },
 })
-// module.exports = {
-//   webpack: (config) => {
-//     // Fixes npm packages that depend on `fs` module
-//     config.node = {
-//       fs: 'empty'
-//     }
 
-//     return config
-//   }
-// }
